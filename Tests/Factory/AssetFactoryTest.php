a8eb115877197bea58edbca5aa5c9ba890ebf990
Oscar Bernardez <oscar.kablex@gmail.com>
2017-08-29 15:30:43 +0200

    Fixed assetic dump command thanks to @qcmnagai

---
+++ Tests/Factory/AssetFactoryTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Hshn\AngularBundle\Tests\Factory;
+
+use Hshn\AngularBundle\Factory\AssetFactory;
+
+class AssetFactoryTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function testParseInputForTemplateCacheAsset()
+    {
+        $kernel = $this->getMockBuilder('Symfony\Component\HttpKernel\KernelInterface')->disableOriginalConstructor()->getMock();
+        $container = $this->getMockBuilder('Symfony\Component\DependencyInjection\ContainerInterface')->disableOriginalConstructor()->getMock();
+        $container
+            ->expects($this->once())
+            ->method('get')
+            ->with($this->equalTo('hshn_angular.asset.template_cache.foo'))
+            ->willReturn('ng_template_cache_foo');
+        $parameterBag = $this->getMockBuilder('Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface')->disableOriginalConstructor()->getMock();
+
+        $factory = new AssetFactory($kernel, $container, $parameterBag, 'path/to/');
+        $parseInput = new \ReflectionMethod($factory, 'parseInput');
+        $parseInput->setAccessible(true);
+
+        $this->assertEquals('ng_template_cache_foo', $parseInput->invoke($factory, '@ng_template_cache_foo', array()));
+    }
+}
